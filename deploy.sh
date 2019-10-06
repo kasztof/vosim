6c3daae26da34dee0308efb2c4eafbdeea3a15b1
kasztof <237986@student.pwr.edu.pl>
2019-10-06 11:23:04 +0200

    Complete deploy process

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-pwd
-ls -la
-scp -rp /home/travis/build/kasztof/vosim root@92.222.69.244:/var/www
+
+ssh root@92.222.69.244 << HERE
+ls /var/www/html
+rm -r /var/www/html/vosim
+ls /var/www/html
+HERE
+
+scp -rp /home/travis/build/kasztof/vosim root@92.222.69.244:/var/www/html
+
+ssh root@92.222.69.244 "service apache2 restart"
