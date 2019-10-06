0959bd2da26316a4a4e84a250f6859fc0b56465b
kasztof <237986@student.pwr.edu.pl>
2019-10-06 11:35:37 +0200

    Add creating venv to deploy script

--- deploy.sh
+++ deploy.sh
@@ -9,4 +9,13 @@ HERE
 
 scp -rp /home/travis/build/kasztof/vosim root@92.222.69.244:/var/www/html
 
-ssh root@92.222.69.244 "service apache2 restart"
+
+
+ssh root@92.222.69.244 << HERE
+cd /var/www/html/vosim
+virtualenv -p python3 venv
+source venv/bin/activate
+pip3 install -r requirements.txt
+deactivate
+service apache2 restart
+HERE
