6e8a70bf3ac0371047d9ed1487f45ed89030d19c
kasztof <237986@student.pwr.edu.pl>
2019-10-20 19:52:00 +0200

    Remove

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,10 +4,9 @@ scp -rp /home/travis/build/kasztof/vosim root@92.222.69.244:/var/www/html
 ssh root@92.222.69.244 cd /var/www/html/vosim
 ssh root@92.222.69.244 cp /etc/vosim/.env .
 
-ssh root@92.222.69.244 app="vosim_app"
-ssh root@92.222.69.244 docker stop ${app}
-ssh root@92.222.69.244 docker rm ${app}
-ssh root@92.222.69.244 docker build -t ${app} .
+ssh root@92.222.69.244 docker stop vosim_app
+ssh root@92.222.69.244 docker rm vosim_app
+ssh root@92.222.69.244 docker build -t vosim_app .
 ssh root@92.222.69.244 docker run -d -p 80:80 \
-  --name=${app} \
-  -v $PWD/app ${app}
+  --name=vosim_app \
+  -v $PWD/app vosim_app
