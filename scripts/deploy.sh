5c9c45192aa56d2806bc799f451d9920ef82b38d
kasztof <237986@student.pwr.edu.pl>
2019-10-20 13:28:53 +0200

    copy env file in the deployment process

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,6 +2,7 @@
 ssh root@92.222.69.244 rm -r /var/www/html/vosim
 scp -rp /home/travis/build/kasztof/vosim root@92.222.69.244:/var/www/html
 ssh root@92.222.69.244 cd /var/www/html/vosim
+ssh root@92.222.69.244 cp /etc/vosim/.env .
 
 app="vosim_app"
 docker stop ${app}
