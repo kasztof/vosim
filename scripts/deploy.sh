9557cc8a96e3dfa16af2f941682630be54c77954
kasztof <237986@student.pwr.edu.pl>
2019-10-20 19:49:12 +0200

    fix

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,7 +4,7 @@ scp -rp /home/travis/build/kasztof/vosim root@92.222.69.244:/var/www/html
 ssh root@92.222.69.244 cd /var/www/html/vosim
 ssh root@92.222.69.244 cp /etc/vosim/.env .
 
-ssh app="vosim_app"
+ssh root@92.222.69.244 app="vosim_app"
 ssh root@92.222.69.244 docker stop ${app}
 ssh root@92.222.69.244 docker rm ${app}
 ssh root@92.222.69.244 docker build -t ${app} .
