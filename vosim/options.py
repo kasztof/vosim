c3cb80ba07199ae2753ece3a7b22f79da18a136b
Krzysztof Kaszanek <krzysztof.kaszanek@kiwee.eu>
2019-11-02 10:45:54 +0100

    Add more graph layouts

--- vosim/options.py
+++ vosim/options.py
@@ -1,10 +1,14 @@
 layout_options = [
     {'label': 'Cose', 'value': 'cose'},
+    {'label': 'Cose-Bilkent', 'value': 'cose-bilkent'},
+    {'label': 'Euler', 'value': 'euler'},
     {'label': 'Klay', 'value': 'klay'},
     {'label': 'Random', 'value': 'random'},
     {'label': 'Circle', 'value': 'circle'},
     {'label': 'Cola', 'value': 'cola'},
-    {'label': 'Dagre', 'value': 'dagre'}
+    {'label': 'Dagre', 'value': 'dagre'},
+    {'label': 'Spread', 'value': 'spread'},
+    {'label': 'Concentric', 'value': 'concentric'}
 ]
 
 model_options = [
