67a3b9473c274a8c47583f16461d5114f5b0a04f
Krzysztof Kaszanek <krzysztof.kaszanek@kiwee.eu>
2019-11-06 23:21:23 +0100

    Fix copying env

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 ssh root@92.222.69.244 rm -r /var/www/html/vosim
 scp -rp /home/travis/build/kasztof/vosim root@92.222.69.244:/var/www/html
+ssh root@92.222.69.244 cp /etc/vosim/.env /var/www/html/vosim
 ssh root@92.222.69.244 cd /var/www/html/vosim
-ssh root@92.222.69.244 cp /etc/vosim/.env .
 
 ssh root@92.222.69.244 docker stop vosim_app
 ssh root@92.222.69.244 docker rm vosim_app
