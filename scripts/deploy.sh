0e0c39b59db6566081f739faebba337dd998f717
Krzysztof Kaszanek <krzysztof.kaszanek@kiwee.eu>
2019-11-07 19:41:49 +0100

    Fix Dockerfile path in deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,5 +6,5 @@ ssh root@92.222.69.244 cd /var/www/html/vosim
 
 ssh root@92.222.69.244 docker stop vosim_app
 ssh root@92.222.69.244 docker rm vosim_app
-ssh root@92.222.69.244 docker build -t vosim_app .
+ssh root@92.222.69.244 docker build -t vosim_app /var/www/html/vosim/
 ssh root@92.222.69.244 docker run -d -p 80:80 --name=vosim_app vosim_app
